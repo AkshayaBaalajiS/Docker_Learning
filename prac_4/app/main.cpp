e7762e99196f45c2efecda6c18d68000e536bbc1
AkshayaBaalajiS <baalaji12akshay@gmail.com>
2026-01-12 09:52:21 +0530

    Prac 4

---
+++ prac_4/app/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "utils.hpp"
+
+int main()
+{
+    std::cout << "Multiply: " << multiply(6, 7) << "\n";
+    return 0;
+}
---
+++ prac_4/lib/utils.cpp
@@ -0,0 +1,6 @@
+#include "utils.hpp"
+
+int multiply(int a, int b)
+{
+    return a * b;
+}
