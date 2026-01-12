87b59931bca41ba2059cdccfc0e7167220231da9
AkshayaBaalajiS <baalaji12akshay@gmail.com>
2026-01-12 08:32:07 +0530

    Dockerfile added refer that for clarity

---
+++ prac_3/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "math_utils.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "CMake Level 1 — Task 3" << endl;
+    cout << "20 + 30 = " << addNumbers(20, 30) << endl;
+    return 0;
+}
