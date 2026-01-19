4cb8db3e3a2dadb2141ce32b6b625056abfb710b
AkshayaBaalajiS <baalaji12akshay@gmail.com>
2026-01-19 09:54:34 +0530

    check the script ad the dockerfile

---
+++ prac_13/binary_literalls.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"This is the program for understanding the binary literal in C++ \n";
+
+	int binary_value = 0b0101; // 5 is 5the value 
+	cout<<"The binary value  = " << binary_value <<endl; 
+}
